85ea1a7d746fea550006211be2e0de17dcc359a5
SamHeatfield1199 <asmirnova1199@gmail.com>
2021-05-25 17:38:01 +1000

    init commit

---
+++ src/components/app.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+
+import SignIn from './pages/sign-in';
+import Flag from './pages/flag';
+import Alert from './utils/alert';
+
+const App = () => {
+  const [accessToken, setAccessToken] = useState(null);
+  const [alerts, setAlerts] = useState([]);
+
+  return (
+    <div className="container">
+      {accessToken ? <Flag /> 
+      : <SignIn setAccessToken={setAccessToken} setAlerts={setAlerts} />}
+      <Alert alertsState={[alerts, setAlerts]} />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/pages/sign-in.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import Input from '../utils/form/input';
+import { Formik, Form } from 'formik';
+import useAsyncAction from '../../hooks/use-async-action/use-async-action';
+
+const SignIn = ({ setAlerts, setAccessToken }) => {
+  const [asyncAction] = useAsyncAction();
+
+  return (
+  
+    
+    <Formik
+      initialValues={{ username: 'formik', password: '' }}
+      onSubmit={async (values) => {
+        // async
+        const config = {
+          method: 'post',
+          url: `/auth/sign-in`,
+          data: { ...values },
+        };
+
+        const data = await asyncAction(config, setAlerts);
+
+        if (data) setAccessToken(data.accessToken);
+      }}>
+      {({ isSubmitting }) => (
+        <Form className="sign-in">
+          <h1 className="sign-in__heading">Sign in</h1>
+          
+          <div className="sign-in mt-m-1">
+            <span className="label mb-s-1">Login: smth</span>
+            <span className="label mb-s-1"> Password: smth</span>
+          </div>
+          <div className="label mb-s-1">Username</div>
+          <Input className="sign-in__input input mb-m-1" type="text" name="username" />
+
+          <div className="label mb-s-1">Password</div>
+          <Input className="sign-in__input input" type="password" name="password" />
+
+          <button
+            disabled={isSubmitting}
+            type="submit"
+            className={`sign-in__btn btn btn--primary btn--flat 
+            ${isSubmitting ? 'btn--spinner btn--submitted' : ''} mt-m-2`}>
+            Submit
+          </button>
+        </Form>
+      )}
+    </Formik>
+
+  );
+};
+
+export default SignIn;
